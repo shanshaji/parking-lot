1eea2fb1a4c495eb656c00c6b832eb2d540779fd
shan <shan@cybrilla.com>
2019-05-24 16:06:01 +0530

    simple parking lot object modelling

--- functional_spec/spec/parking_lot_spec.rb
+++ functional_spec/spec/parking_lot_spec.rb
@@ -21,18 +21,4 @@ RSpec.describe 'Parking Lot' do
     run_command(pty, "leave 1\n")
     expect(fetch_stdout(pty)).to end_with("Slot number 1 is free\n")
   end
-
-  it "can report status" do
-    run_command(pty, "park KA-01-HH-1234 White\n")
-    run_command(pty, "park KA-01-HH-3141 Black\n")
-    run_command(pty, "park KA-01-HH-9999 White\n")
-    run_command(pty, "status\n")
-    expect(fetch_stdout(pty)).to end_with(<<-EOTXT
-Slot No./t Registration No/t Colour
-1/t/t KA-01-HH-1234/t/t White
-2/t/t KA-01-HH-3141/t/t Black
-3/t/t KA-01-HH-9999/t/t White
-EOTXT
-)
-  end
 end
